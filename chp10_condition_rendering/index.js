c769a2a0b4fd39814c86c92c3c7fd2d5565b8954
木村　幸介 <kosukekimura0528@gmail.com>
2019-03-18 10:25:42 +0900

    study chp9 / chp10

---
+++ chp10_condition_rendering/index.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: '#app',
+  data:{
+    toggle:false,
+    color:"yew",
+    showToggle:true,
+
+
+  }
+})
+
+console.log("OK")
---
+++ chp9_binding/index.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+  el:"#app",
+  data:{
+    isLarge:true,
+    hasError:true,
+
+    largeClass:'large',
+    dangerClass:'text-danger',
+
+    classObject:{
+      large:true,
+      'text-danger':true
+    },
+
+    largeClass:{
+      large:true,
+      'bg-gray':true
+    },
+    dangerClass :{
+      'text-danger':true
+    },
+    isLarge:false,
+
+    color:'blue',
+    fontSize:36,
+
+    styleObject:{
+      color:'blue',
+      fontSize:'36px'
+    }
+
+  },
+})
